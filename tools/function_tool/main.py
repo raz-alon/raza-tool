c05c56046c97c2a3072eb04c54d344fd3abd5dd4
raz-alon <raz.alon@kubiya.ai>
2024-10-30 16:44:25 +0200

    update tool

--- tools/function_tool/main.py
+++ tools/function_tool/main.py
@@ -1,6 +1,5 @@
-from typing_extensions import Annotated
-
 import typer
+from typing_extensions import Annotated
 
 from kubiya_sdk.tools import function_tool
 
@@ -12,15 +11,11 @@ from kubiya_sdk.tools import function_tool
 def test_123(
     name: str,
     boolean_val: bool,  # This will validate that the input is a boolean
-    optional_str: Annotated[
-        str, typer.Argument()
-    ] = "sheeesh",  # This is how to add a default value
+    optional_str: Annotated[str, typer.Argument()] = "sheeesh",  # This is how to add a default value
 ):
     import pandas as pd
 
     print(f"Hello {name}! {boolean_val} {optional_str}")
-    df = pd.DataFrame(
-        {"name": [name], "boolean_val": [boolean_val], "test": [optional_str]}
-    )
+    df = pd.DataFrame({"name": [name], "boolean_val": [boolean_val], "test": [optional_str]})
 
     print(df)
--- tools/hello_world_tool/tool_def.py
+++ tools/hello_world_tool/tool_def.py
@@ -1,31 +1,28 @@
-import os
-
-from . import main
 import inspect
 
-
-from kubiya_sdk.tools.models import Tool, Arg, FileSpec
+from kubiya_sdk.tools.models import Arg, Tool, FileSpec
 from kubiya_sdk.tools.registry import tool_registry
 
-def _get_requirements_content() -> str:
-    with open(os.path.join(os.path.dirname(__file__), '../../requirements.txt'), 'r') as f:
-        return f.read()
+from . import main
 
 hello_tool = Tool(
-    name="say_hello2",
+    name="say_hello",
     type="docker",
     image="python:3.12",
     description="Prints hello {name}!",
     args=[Arg(name="name", description="name to say hello to", required=True)],
-    content="""
+    on_build="""
 curl -LsSf https://astral.sh/uv/install.sh | sh > /dev/null 2>&1
 . $HOME/.cargo/env
 
 uv venv > /dev/null 2>&1
 . .venv/bin/activate > /dev/null 2>&1
 
-uv pip install -r /tmp/requirements.txt > /dev/null 2>&1
-
+if [ -f /tmp/requirements.txt ]; then
+    uv pip install -r /tmp/requirements.txt > /dev/null 2>&1
+fi
+""",
+    content="""
 python /tmp/main.py "{{ .name }}"
 """,
     with_files=[
@@ -33,11 +30,12 @@ python /tmp/main.py "{{ .name }}"
             destination="/tmp/main.py",
             content=inspect.getsource(main),
         ),
-        FileSpec(
-            destination="/tmp/requirements.txt",
-            content=_get_requirements_content(),
-        ),
+        # Add any requirements here if needed
+        # FileSpec(
+        #     destination="/tmp/requirements.txt",
+        #     content="",
+        # ),
     ],
 )
 
-tool_registry.register("hello", hello_tool)
+tool_registry.register(hello_tool)
